02706f4d511c8fd178717713b8f32aa22125f013
Your Name <you@example.com>
2018-07-07 15:11:01 +0300

    ddd

--- vars/deployToS3.groovy
+++ vars/deployToS3.groovy
@@ -1,4 +1,14 @@
 def call(Map config)
 {
     echo config.file
+
+    withAWS(credentials: 'AWSCredentials', region: 'eu-west-1') {
+          timeout(time: 3, unit: 'MINUTES') {
+            retry(count: 5) {
+              s3Upload(file: ZipPackageName, bucket: AWSBucket, path: "Test${WorkspaceName}/${ZipPackageName}")
+            }
+
+          }
+
+        }
 }
