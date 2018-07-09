1e54dc5548fd9a9fa6e2341985ba846ca83555d0
Your Name <you@example.com>
2018-07-09 11:45:23 +0300

    Try using indexed method

--- vars/archivePackage.groovy
+++ vars/archivePackage.groovy
@@ -2,7 +2,7 @@ def call()
 {
     def properties = readJSON file: ConfigLocation
 
-    def includes = properties.Artifacts.Includes.toList()
+    def includes = properties.Artifacts.Includes.toList().indexed()
 
     def artifacts = ""
 
