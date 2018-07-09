ffc693b679dd14dcbb67f48ae9b56add0e0897d4
Your Name <you@example.com>
2018-07-09 11:58:05 +0300

    Cal extra to list

--- vars/archivePackage.groovy
+++ vars/archivePackage.groovy
@@ -2,7 +2,7 @@ def call()
 {
     def properties = readJSON file: ConfigLocation
 
-    def includes = properties.Artifacts.Includes.toList().indexed()
+    def includes = properties.Artifacts.Includes.toList().toList()
 
     def artifacts = ""
 
