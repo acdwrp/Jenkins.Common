264a1a3f4e96e7b95b84eb48e65b11f13498c1b5
Your Name <you@example.com>
2018-07-09 13:17:03 +0300

    vvv

--- vars/archivePackage.groovy
+++ vars/archivePackage.groovy
@@ -4,25 +4,16 @@ def call()
 
     def includes = properties.Artifacts.Includes.toList()
 
-    def artifacts1 = ""
-
-    def type = includes.getClass()
-
-    echo "${type}"
+    
 
     def sub = includes.subList(0, includes.size() -1)
 
-    def type2 = sub.getClass()
-
-    echo "${type2}"
-
-    for (i = 0; i< includes.size(); i++) {
+    for (i = 0; i< sub.size(); i++) {
        def incl = sub[i]
 
        echo incl
     }
 
-    echo artifacts1
 
     zip(glob: properties.ArtifactsToPack, zipFile: properties.ZipPackageName)
     archiveArtifacts(artifacts: properties.ZipPackageName, onlyIfSuccessful: true, fingerprint: true)
