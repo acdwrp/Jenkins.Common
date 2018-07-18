b65a102924d3e714046d50d711a0cda4a1d020f7
Your Name <you@example.com>
2018-07-18 11:14:23 +0300

    metafile test

--- vars/deployToS3.groovy
+++ vars/deployToS3.groovy
@@ -6,6 +6,13 @@ def call()
           timeout(time: 3, unit: 'MINUTES') {
             retry(count: 5) {
               s3Upload(file: properties.ZipPackageName, bucket: properties.AWSBucket, path: "${WorkspaceName}/${properties.ZipPackageName}")
+
+              // Write an useful file, which is needed to be archived.
+              def metaFile = "${WorkspaceName}/meta.json"
+
+              writeFile file: metaFile, text: "This file is useful, need to archive it. ${BUILD_NUMBER}"
+
+              s3Upload(file: metaFile, bucket: properties.AWSBucket, path: "${WorkspaceName}/meta.json")
             }
 
           }
