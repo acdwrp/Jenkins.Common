e3e6c9eb1cf845ab2bf152818f4e3643fdb1f5c9
Your Name <you@example.com>
2018-07-19 08:58:53 +0300

    asd

--- vars/deployToS3.groovy
+++ vars/deployToS3.groovy
@@ -6,15 +6,7 @@ def call()
           timeout(time: 3, unit: 'MINUTES') {
             retry(count: 5) {
               s3Upload(file: properties.ZipPackageName, bucket: properties.AWSBucket, path: "${WorkspaceName}/${properties.ZipPackageName}")
-
-              // Write an useful file, which is needed to be archived.
-              def metaFile = "meta.json"
-
-              writeFile file: metaFile, text: "{ \"LatestVersion\" :  ${BUILD_NUMBER}}"
-
-              s3Upload(file: metaFile, bucket: properties.AWSBucket, path: "${WorkspaceName}/meta.json")
             }
-
           }
         }
 }
--- vars/pushToEnvironment.groovy
+++ vars/pushToEnvironment.groovy
@@ -1,4 +1,16 @@
 def call()
 {
-    
+    def properties = readJSON file: ConfigLocation
+
+    def metaFile = "meta.json"
+
+    withAWS(credentials: properties.AWSCredentials, region: properties.AWSRegion) {
+        timeout(time: 3, unit: 'MINUTES') {
+            retry(count: 5) {
+              writeFile file: metaFile, text: "{ \"LatestVersion\" :  ${BUILD_NUMBER}}"
+
+              s3Upload(file: metaFile, bucket: properties.AWSBucket, path: "${WorkspaceName}/meta.json")
+            }
+          }
+        }       
 }
